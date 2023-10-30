7b92b83f0b597b4dc5b564f5c2a7cd79d9e33464
kavitavkokate <147817308+kavitavkokate@users.noreply.github.com>
2023-10-30 16:24:00 +0000

    Added unit tests

--- src/test/java/com/onlineapp/restaurantlisting/RestaurantListingApplicationTests.java
+++ src/test/java/com/onlineapp/restaurantlisting/RestaurantListingApplicationTests.java
@@ -1,13 +1,12 @@
-package com.onlineapp.restaurantlisting;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RestaurantListingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.onlineapp.restaurantlisting;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class RestaurantListingApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
